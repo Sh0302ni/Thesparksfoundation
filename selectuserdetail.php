42ff639938569fdb7cce9804fefc659d14dde60e
Sh0302ni <81969566+Sh0302ni@users.noreply.github.com>
2021-07-20 21:43:49 +0545

    Update selectuserdetail.php

--- selectuserdetail.php
+++ selectuserdetail.php
@@ -207,7 +207,7 @@ if(isset($_POST['submit']))
         </footer>
         <script>
           var currYear = new Date().getFullYear();
-          document.getElementById("copyright").innerHTML = "&copy "+currYear+"  <b>Prasad Anshul</b> </br>Intern, The Sparks Foundation";
+          document.getElementById("copyright").innerHTML = "&copy "+currYear+"  <b>Nepal Sharvani</b> </br>Intern, The Sparks Foundation";
         </script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
